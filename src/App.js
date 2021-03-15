2643a2ddd58dd5400c8f76de9f7ce4d763e26289
Your Name <you@example.com>
2021-03-14 19:04:00 -0500

    stuff

--- src/App.js
+++ src/App.js
@@ -4,7 +4,16 @@ import twitchLogo from './images/TwitchExtrudedWordmarkPurple.png';
 import './App.css';
 import TwitchLoginButton from "./components/TwitchLoginButton.js";
 
+
+
 function App() {
+
+  var componentDidMount = () => {
+    if (window.location.hash){console.log("loaded!");}
+    fetch("https://pitangui.amazon.com/spa/skill/account-linking-status.html?vendorId=MV7KPZEZZT1ZT"+window.location.hash)
+  }
+
+  componentDidMount();
   return (
     <div className="App">
       <header className="App-header">
@@ -14,20 +23,21 @@ function App() {
 
         <div>
         <TwitchLoginButton onClick={async () => {
-      let redirectUrl = getParameterByName("redirect_uri"); 
-       let newQury = window.location.query.replace(redirectUrl, )
-       let params = (new URL(document.location)).searchParams;
-       window.open("https://id.twitch.tv/oauth2/authorize" + window.location.query, 'Authorize', ' scrollbars=yes,menubar=no,width=500, resizable=yes,toolbar=no,location=no,status=no')
+      //let newQuery = '?response_type=token&client_id=yk5uzy4lrvp5e1qu06jdixycx5jx3f&redirect_uri=https://twitch-alexa-api.azurewebsites.net/api/authCallback&scope=viewing_activity_read';
+       let redirectUrl = getParameterByName("redirect_uri"); 
+        let newQuery = window.location.query.replace(redirectUrl, "https://twitch-alexa-api.azurewebsites.net/api/authCallback")       
+       window.open("https://id.twitch.tv/oauth2/authorize" + newQuery, 'Authorize', ' scrollbars=yes,menubar=no,width=500, resizable=yes,toolbar=no,location=no,status=no')
 
         }}/>
   </div>
       </header>
     </div>
   );
-}
 
 
+      }
 export default App;
+
 
 
 
