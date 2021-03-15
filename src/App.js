f1f82a0a8289a783eaa4f565488954b2496d697e
Your Name <you@example.com>
2021-03-14 19:13:57 -0500

    redirect

--- src/App.js
+++ src/App.js
@@ -24,7 +24,8 @@ function App() {
         <div>
         <TwitchLoginButton onClick={async () => {
       //let newQuery = '?response_type=token&client_id=yk5uzy4lrvp5e1qu06jdixycx5jx3f&redirect_uri=https://twitch-alexa-api.azurewebsites.net/api/authCallback&scope=viewing_activity_read';
-       let redirectUrl = getParameterByName("redirect_uri"); 
+      const urlParams = new URLSearchParams(window.location.search);
+      const redirectUrl = urlParams.get('redirect_uri');
         let newQuery = window.location.query.replace(redirectUrl, "https://twitch-alexa-api.azurewebsites.net/api/authCallback")       
        window.open("https://id.twitch.tv/oauth2/authorize" + newQuery, 'Authorize', ' scrollbars=yes,menubar=no,width=500, resizable=yes,toolbar=no,location=no,status=no')
 
